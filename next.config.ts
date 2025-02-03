68debdf56b35a0bcddb950e297fcf82aa175c72f
paakowden <paakow1114@gmail.com>
2025-02-03 09:45:38 +0000

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  //distDir: "build",
-  output: "export",
+  distDir: "build",
+  //output: "export",
   images: { unoptimized: true },
 };
 
